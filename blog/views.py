c4c364a08c06c3c857aa8e81b2037ea981563bf2
claudia-duda <claudiaeduarda_duda@hotmail.com>
2020-03-21 14:30:46 -0300

    the first template to blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    # methoud request and return the model for render , where mont the model  html
+    

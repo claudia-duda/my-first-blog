94179840f7f4ec320d5b934f0b1712d185072365
claudia-duda <claudiaeduarda_duda@hotmail.com>
2020-04-01 17:46:42 -0300

    modified Procfile error

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +24,9 @@ LOGIN_REDIRECT_URL = '/'
 SECRET_KEY = 'b=sx&_6%t1(mtas_)^ee@cn7ogjh531#)d4fbw#nw!psif5nu#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1' , '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1' , '.herokuapp.com']
 
 
 # Application definition
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'adm',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'',
+    }    
 }
 
 
@@ -121,3 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#/////////////////////////////////////////////////////////////////////////////////////
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
